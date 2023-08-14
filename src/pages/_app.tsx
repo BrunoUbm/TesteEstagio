5db642c9d14de6e434460a76edd455524aec0ab8
BrunoUbm <brunoubaldo10@gmail.com>
2023-08-14 08:25:23 -0300

    Global styles add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,10 @@ import type { AppProps } from 'next/app';
 import { GlobalStyles } from '@/components/Global.style';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  );
+    return (
+        <>
+            <GlobalStyles />
+            <Component {...pageProps} />
+        </>
+    );
 }
-
-
